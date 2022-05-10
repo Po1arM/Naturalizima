b35e232bdcdc60e42fb37cffcc031b0eb7607a39
Felix Guzman <felix1020304@gmail.com>
2022-05-10 14:18:44 -0400

    Version Final

---
+++ src/test/java/clinica/naturalizima/NaturalizimaApplicationTests.java
@@ -0,0 +1,13 @@
+package clinica.naturalizima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalizimaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
